868a701f440aae0ccf6fd163f839ec34d9c4891a
Helisan Florindo <helisboyflorindo@gmail.com>
2026-08-18 18:36:35 +0100

    aumenta limite dos comprovativos para 15MB

--- config/upload.js
+++ config/upload.js
@@ -14,8 +14,8 @@ const storage = new CloudinaryStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024,
-  },
+  fileSize: 15 * 1024 * 1024,
+},
 });
 
 module.exports = upload;
