4ad7100f5233feda9c318540b8a686434a410bf4
Helisan Florindo <helisboyflorindo@gmail.com>
2026-08-19 17:43:29 +0100

    fix: configurar upload de comprovativos com Cloudinary

---
+++ src/config/upload.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'controle_motoristas/despesas',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+        resource_type: 'image'
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
+
+module.exports = upload;
