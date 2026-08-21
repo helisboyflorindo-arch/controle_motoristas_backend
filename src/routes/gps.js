16724bd9dab5897baaec011f0c229a0c1019b9b0
Helisan Florindo <helisboyflorindo@gmail.com>
2026-08-21 22:03:06 +0100

    Adicionar calculo de quilometragem por GPS

--- src/routes/gps.js
+++ src/routes/gps.js
@@ -3,6 +3,42 @@ const router = express.Router();
 
 const pool = require('../db');
 
+function calcularDistancia(lat1, lon1, lat2, lon2) {
+
+    const R = 6371; // km
+
+    const dLat = 
+        (lat2 - lat1) * Math.PI / 180;
+
+    const dLon =
+        (lon2 - lon1) * Math.PI / 180;
+
+
+    const a =
+        Math.sin(dLat/2) *
+        Math.sin(dLat/2) +
+
+        Math.cos(lat1 * Math.PI / 180) *
+        Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon/2) *
+        Math.sin(dLon/2);
+
+
+    const c =
+        2 * Math.atan2(
+            Math.sqrt(a),
+            Math.sqrt(1-a)
+        );
+
+
+    return R * c;
+
+}
+
+
+
+
+
 
 // ======================================
 // RECEBER GPS DO MOTORISTA
@@ -18,6 +54,59 @@ router.post('/enviar', async (req, res) => {
             longitude,
             velocidade
         } = req.body;
+
+        const [ultimo] = await pool.query(
+            `
+            SELECT latitude, longitude
+            FROM localizacoes
+            WHERE motorista_id = ?
+            ORDER BY id DESC
+            LIMIT 1
+            `,
+            [motorista_id]
+            );
+
+            let distancia = 0;
+
+
+            if(ultimo.length > 0){
+
+            distancia = calcularDistancia(
+                Number(ultimo[0].latitude),
+                Number(ultimo[0].longitude),
+                Number(latitude),
+                Number(longitude)
+            );
+
+            }
+            if(distancia < 0.01){
+
+ distancia = 0;
+
+}
+
+if(distancia > 0){
+
+await pool.query(
+`
+INSERT INTO motorista_km
+(motorista_id,data,km)
+
+VALUES(?,?,?)
+
+ON DUPLICATE KEY UPDATE
+
+km = km + VALUES(km)
+
+`,
+[
+motorista_id,
+new Date(),
+distancia
+]
+);
+
+}
 
 
         if (
